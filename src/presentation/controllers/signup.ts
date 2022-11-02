f2e01b8ade7fdbf9d70871028d0c1d5449b85272
Gustavo <gjbaltieri@gmail.com>
2022-11-02 10:31:42 -0300

    feat: return 400 if passwordConfirmation not match

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -16,7 +16,11 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
-      const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
+      const { email, password, passwordConfirmation } = httpRequest.body
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+      const emailIsValid = this.emailValidator.isValid(email)
       if (!emailIsValid) {
         return badRequest(new InvalidParamError('email'))
       }
