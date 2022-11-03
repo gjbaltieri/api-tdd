c4e3a4b017bc06114c53f5fc570485735693c141
Gustavo <gjbaltieri@gmail.com>
2022-11-03 12:32:07 -0300

    chore: add jest config and scripts

--- src/presentation/protocols/index.ts
+++ src/presentation/protocols/index.ts
@@ -1,2 +1,3 @@
-export * from './controller'
-export * from './http'
+// Poderia estar como export * from './controller'... mas o coverage do jest não reconhece e fica em 0%
+export { Controller } from './controller'
+export { HttpRequest, HttpResponse } from './http'
