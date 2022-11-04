5e9b669bfb22b6fb45306fdee73561d96ca388ec
Gustavo <gjbaltieri@gmail.com>
2022-11-04 09:40:37 -0300

    refactor: move sut create to a makeSut method

--- src/data/usecases/dbAddAccount.spec.ts
+++ src/data/usecases/dbAddAccount.spec.ts
@@ -1,16 +1,26 @@
+import { Encrypter } from '../protocols/Encrypt'
 import { DbAddAccount } from './dbAddAccount'
 
-class EncryptStub {
-  async encrypt (password: string): Promise<string> {
-    return new Promise(resolve => resolve('hashad_password'))
+interface SutTypes {
+  sut: DbAddAccount
+  encrypterStub: Encrypter
+}
+const makeSut = (): SutTypes => {
+  class EncryptStub {
+    async encrypt (password: string): Promise<string> {
+      return new Promise(resolve => resolve('hashad_password'))
+    }
   }
+  const encrypterStub = new EncryptStub()
+  const sut = new DbAddAccount(encrypterStub)
+
+  return { sut, encrypterStub }
 }
 
 describe('DbAddAccount usecase', () => {
   test('Should call Encrypt with correct password', async () => {
-    const encryptStub = new EncryptStub()
-    const sut = new DbAddAccount(encryptStub)
-    const encryptSpy = jest.spyOn(encryptStub, 'encrypt')
+    const { sut, encrypterStub } = makeSut()
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     const accountData = {
       name: 'valid_name',
       email: 'valid_email@mail.com',
