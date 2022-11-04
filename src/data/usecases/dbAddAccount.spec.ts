3dd59acbfa8399961c0cfd6f034d20915be267fd
Gustavo <gjbaltieri@gmail.com>
2022-11-04 12:37:10 -0300

    test: ensure AddAccount return an account on sucess

--- src/data/usecases/dbAddAccount.spec.ts
+++ src/data/usecases/dbAddAccount.spec.ts
@@ -90,4 +90,21 @@ describe('DbAddAccount usecase', () => {
     const promise = sut.add(accountData)
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should AddAccount return an account on sucess', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    console.log(account)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'hashad_password'
+    })
+  })
 })
