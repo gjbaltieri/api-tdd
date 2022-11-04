2ef2819e0aea17c0a0c0f2f5651a9d6a48e0055f
Gustavo <gjbaltieri@gmail.com>
2022-11-04 09:44:41 -0300

    refactor: move EncrypterStub to a makeEncrypter helper  method

--- src/data/usecases/dbAddAccount.spec.ts
+++ src/data/usecases/dbAddAccount.spec.ts
@@ -5,13 +5,16 @@ interface SutTypes {
   sut: DbAddAccount
   encrypterStub: Encrypter
 }
-const makeSut = (): SutTypes => {
-  class EncryptStub {
+const makeEncrypter = (): Encrypter => {
+  class EncryptStub implements Encrypter {
     async encrypt (password: string): Promise<string> {
       return new Promise(resolve => resolve('hashad_password'))
     }
   }
-  const encrypterStub = new EncryptStub()
+  return new EncryptStub()
+}
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncrypter()
   const sut = new DbAddAccount(encrypterStub)
 
   return { sut, encrypterStub }
