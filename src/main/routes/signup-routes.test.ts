d0ca5759811e02e4107c3ec85cc3a3985115ffe9
Gustavo <gjbaltieri@gmail.com>
2022-11-07 17:16:29 -0300

    refactor: use MongoHelper beforeAll tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,18 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helper/mongo-helper'
 import app from '../config/app'
 
 describe('SignUp route', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    const collection = MongoHelper.getCollection('accounts')
+    await collection.deleteMany({})
+  })
   test('should return an account on sucess', async () => {
     await request(app).post('/api/signup').send()
       .expect(200)
