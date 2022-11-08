4508e659f6209d89d20506fa10c43e89115df16c
Gustavo <gjbaltieri@gmail.com>
2022-11-08 16:42:23 -0300

    refactor: move sut creation to a helper method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,17 +1,33 @@
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
 
+interface IMakeSut {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpReponse: HttpResponse = { statusCode: 200, body: httpRequest.body }
+      return httpReponse
+    }
+  }
+  const controllerStub = new ControllerStub()
+  return controllerStub
+}
+const makeSut = (): IMakeSut => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut,
+    controllerStub
+  }
+}
+
 describe('Log Decorator', () => {
   test('should call controller handler', async () => {
-    class ControllerStub implements Controller {
-      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpReponse: HttpResponse = { statusCode: 200, body: httpRequest.body }
-        return httpReponse
-      }
-    }
-    const controllerStub = new ControllerStub()
+    const { controllerStub, sut } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
-    const sut = new LogControllerDecorator(controllerStub)
     const httpRequest = {
       body: {
         name: 'any_name',
