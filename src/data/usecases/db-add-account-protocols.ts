7fa805ebecbb6e20e04ae4324d616e4a9186dac2
Gustavo <gjbaltieri@gmail.com>
2022-11-09 11:11:50 -0300

    feat: ensure LogErrorRepository calls with correct param in LogControllerDecorator

--- src/data/usecases/db-add-account-protocols.ts
+++ src/data/usecases/db-add-account-protocols.ts
@@ -1,4 +1,4 @@
 export { AccountModel } from '../../domain/models/account'
 export { AddAccount, AddAccountModel } from '../../domain/usecases/addAccount'
 export { Encrypter } from '../protocols/Encrypt'
-export { AddAccountRepository } from '../protocols/add-account-repository'
+export { AddAccountRepository } from '../protocols/log-error-repository'
--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,13 +1,19 @@
+import { LogErrorRepository } from '../../data/protocols/log-error-repository'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 
 export class LogControllerDecorator implements Controller {
   private readonly controller: Controller
-  constructor (controller: Controller) {
+  private readonly logRepository: LogErrorRepository
+  constructor (controller: Controller, logRepository: LogErrorRepository) {
     this.controller = controller
+    this.logRepository = logRepository
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
+    if (httpResponse.statusCode === 500) {
+      await this.logRepository.log(httpResponse.body.stack)
+    }
     return httpResponse
   }
 }
