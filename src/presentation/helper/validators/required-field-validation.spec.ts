51dd76efb1dd6919a18a789c4360552d9356ad17
Gustavo <gjbaltieri@gmail.com>
2022-11-11 08:44:48 -0300

    test: ensure a RequiredField Validation return error type of MissingParamError

---
+++ src/presentation/helper/validators/required-field-validation.spec.ts
@@ -0,0 +1,10 @@
+import { MissingParamError } from '../../errors'
+import { RequiredFieldValidation } from './requiered-field-validation'
+
+describe('RequiredField Validation', () => {
+  test('should return a MissingParam error if validation fails', () => {
+    const fieldValidation = new RequiredFieldValidation('expect_field')
+    const error = fieldValidation.validate({ anotherField: 'another_field' })
+    expect(error).toEqual(new MissingParamError('expect_field'))
+  })
+})
