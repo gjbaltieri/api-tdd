9d04a8f760518342af4bfcbf8d63b0ad6e691c68
Gustavo <gjbaltieri@gmail.com>
2022-11-11 08:48:48 -0300

    refactor: move sut creation to a helper method

--- src/presentation/helper/validators/required-field-validation.spec.ts
+++ src/presentation/helper/validators/required-field-validation.spec.ts
@@ -1,16 +1,20 @@
 import { MissingParamError } from '../../errors'
 import { RequiredFieldValidation } from './requiered-field-validation'
 
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('expect_field')
+}
+
 describe('RequiredField Validation', () => {
   test('should return a MissingParam error if validation fails', () => {
-    const fieldValidation = new RequiredFieldValidation('expect_field')
-    const error = fieldValidation.validate({ anotherField: 'another_field' })
+    const fieldValidation = makeSut()
+    const error = fieldValidation.validate({ anotherField: 'any_value' })
     expect(error).toEqual(new MissingParamError('expect_field'))
   })
 
   test('should not return if validation sucess', () => {
-    const fieldValidation = new RequiredFieldValidation('expect_field')
-    const error = fieldValidation.validate({ anotherField: 'another_field' })
-    expect(error).toEqual(new MissingParamError('expect_field'))
+    const fieldValidation = makeSut()
+    const error = fieldValidation.validate({ expect_field: 'any_value' })
+    expect(error).toBeFalsy()
   })
 })
