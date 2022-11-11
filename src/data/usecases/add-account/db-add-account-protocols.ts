4c480783ea43fec1d9d99af26010ea322c2d0932
Gustavo <gjbaltieri@gmail.com>
2022-11-11 13:57:33 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -0,0 +1,4 @@
+export { AccountModel } from '../../../domain/models/account'
+export { AddAccount, AddAccountModel } from '../../../domain/usecases/addAccount'
+export { Encrypter } from '../../protocols/Encrypt'
+export { AddAccountRepository } from '../../protocols/add-account-repository'
--- src/data/usecases/db-add-account-protocols.ts
+++
@@ -1,4 +0,0 @@
-export { AccountModel } from '../../domain/models/account'
-export { AddAccount, AddAccountModel } from '../../domain/usecases/addAccount'
-export { Encrypter } from '../protocols/Encrypt'
-export { AddAccountRepository } from '../protocols/add-account-repository'
