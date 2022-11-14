beab2739b3ce1a140bcb2e732ca8e479e942267d
Gustavo <gjbaltieri@gmail.com>
2022-11-14 10:54:42 -0300

    test: ensure AccountMongoRepository returns an account if loadByEmail sucess

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,3 +1,4 @@
+import { Collection } from 'mongodb'
 import { MongoHelper } from '../helper/mongo-helper'
 import { AccountMongoRepository } from './account'
 
@@ -5,6 +6,7 @@ const makeSut = (): AccountMongoRepository => {
   return new AccountMongoRepository()
 }
 
+let collection: Collection
 describe('Account mongodb repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -13,11 +15,11 @@ describe('Account mongodb repository', () => {
     await MongoHelper.disconnect()
   })
   beforeEach(async () => {
-    const collection = await MongoHelper.getCollection('accounts')
+    collection = await MongoHelper.getCollection('accounts')
     await collection.deleteMany({})
   })
 
-  test('should return account on sucess', async () => {
+  test('should return account on add sucess', async () => {
     const sut = makeSut()
     const account = await sut.add({
       name: 'any_name',
@@ -30,5 +32,21 @@ describe('Account mongodb repository', () => {
     expect(account.name).toBe('any_name')
     expect(account.email).toBe('any_email@mail.com')
     expect(account.password).toBe('any_password')
+  })
+
+  test('should return account on loadAccountByEmail sucess', async () => {
+    const sut = makeSut()
+    await collection.insertOne({
+      name: 'mock_name',
+      email: 'mock_email@mail.com',
+      password: 'mock_password'
+    })
+    const account = await sut.loadByEmail('mock_email@mail.com')
+    expect(account).toBeTruthy()
+    expect(account).toHaveProperty('id')
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('mock_name')
+    expect(account.email).toBe('mock_email@mail.com')
+    expect(account.password).toBe('mock_password')
   })
 })
